05013ed58ae45bfba2bf173d03bafee7b74f29ce
kadenjenson <sobeit99@gmail.com>
2018-05-16 10:39:22 -0600

    Added onto instructor challenge

--- JavaScript/instructor.js
+++ JavaScript/instructor.js
@@ -1,8 +1,15 @@
 class Instructor {
-	constructor({ name }) {
+	constructor({ name, role = 'Assistant TO the regional manager' }) {
 		this.name = name;
+		this.role = role;
+	}
+
+	renderDetails() {
+		console.log(`${this.name}: ${this.role}`)
 	}
 }
 
-const jon = new Instructor({ name: 'Jon Snow'});
-console.log(jon.name);
+const dwight = new Instructor({ name: 'Dwight Shrute'});
+const jon = new Instructor({ name: 'Jon Snow', role: 'Rightful heir to the throne'});
+dwight.renderDetails();
+jon.renderDetails();
