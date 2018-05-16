9fe6af9334142266e453bbd9858bfd13e8e2dcd2
kadenjenson <sobeit99@gmail.com>
2018-05-16 11:59:59 -0600

    Added static methods to instructor challenge

--- JavaScript/instructor.js
+++ JavaScript/instructor.js
@@ -7,9 +7,28 @@ class Instructor {
 	renderDetails() {
 		console.log(`${this.name}: ${this.role}`)
 	}
+
+	static helloWorld() {
+		console.log('Hi there');
+	}
+
+	static canTeach(instructor) {
+		return (instructor.role === 'classroom');
+	}
 }
 
+// Instructor.helloWorld();
+
 const dwight = new Instructor({ name: 'Dwight Shrute'});
-const jon = new Instructor({ name: 'Jon Snow', role: 'Rightful heir to the throne'});
-dwight.renderDetails();
-jon.renderDetails();
+console.log(
+	`${dwight.name} can teach: ${Instructor.canTeach(dwight)}`
+	);
+
+const kaden = new Instructor({ name: 'Kaden', role: 'classroom' });
+console.log(
+	`${kaden.name} can teach: ${Instructor.canTeach(kaden)}`
+	);
+
+// const jon = new Instructor({ name: 'Jon Snow', role: 'Rightful heir to the throne'});
+// dwight.renderDetails();
+// jon.renderDetails();
